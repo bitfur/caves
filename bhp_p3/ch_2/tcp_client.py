f4746469ea58df7ae8fb320838a8157a1bbb42a1
BitFur <bitfur@users.noreply.github.comv>
2021-01-07 03:05:41 +0200

    TCP Socket server/client example

--- bhp_p3/ch_2/tcp_client.py
+++ bhp_p3/ch_2/tcp_client.py
@@ -1,9 +1,10 @@
 import socket
-target_host = "www.google.com"
-target_port = 80
+target_host = "localhost"
+target_port = 9999
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-request = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+request = "sample data"
+print("[*] Seding data: {}".format(request))
 client.send(request.encode())
 response = client.recv(4096)
-print(response)
+print("[*] Response data: {}".format(response.decode()))
